861fefbe427ca6a0f12372340d235d29774714d5
Aswathy N <aswathia.achu@gmail.com>
2022-11-26 15:16:16 +0530

    homepage with sidebar

--- src/components/Navigation/PublicNavbar.jsx
+++ src/components/Navigation/PublicNavbar.jsx
@@ -103,6 +103,7 @@
 import React, { useState } from 'react'
 import { Disclosure } from "@headlessui/react";
 // import { Link } from "react-router-dom";
+import Sidebar from '../Sidebar';
 
 import {
     MenuIcon,
@@ -145,16 +146,16 @@ const PublicNavbar = () => {
     return (
         <Disclosure
             as="nav"
-            className="container bg-orange-700 shadow-md shadow-gray-300 sticky top-0 z-50 "
+            className="container md:bg-orange-700 shadow-md shadow-gray-300 sticky top-0 z-50 "
         >
             {({ open }) => (
                 <>
-                    <div className=" container px-4 sm:px-6 lg:px-8 h-[100px] ">
+                    <div className=" container px-4 sm:px-8 lg:px-8 h-[100px] ">
                         <div className="flex  h-24">
                             <div className="flex ">
                                 <div className="-ml-2 mr-2 flex items-center md:hidden ">
                                     {/* Mobile menu button */}
-                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-black sm:justify-start md:text-white hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                         {/* <span className="sr-only">Open main menu</span> */}
                                         {open ? (
                                             <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -174,7 +175,7 @@ const PublicNavbar = () => {
                                 </div>
 
                                 <div className=" grid grid-rows-2 grid-flow-col w-auto ml-[500px] gap-x-0.5 ">
-                                    <ul className={`gap-4 md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-700 md:z-auto z-[-1] left-0 md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'top:8rem ' : 'top-[-490px]'}`}>
+                                    <ul className={`gap-4 justify-end sm: hidden md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-700 md:z-auto z-[-1] left-0 md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'top:8rem ' : 'top-[-490px]'}`}>
                                         {
                                             Links.map((link) => (
                                                 <li key={link.name}>
@@ -186,13 +187,13 @@ const PublicNavbar = () => {
                                         }
 
                                     </ul>
-                                    <ul className={`justify-end gap-4 md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-700 md:z-auto z-[-1] left-0 md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'top:8rem ' : 'top-[-490px]'}`}>
+                                    <ul className={`gap-4 justify-end sm: hidden md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-700 md:z-auto z-[-1] left-0 md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'top:8rem ' : 'top-[-490px]'}`}>
                                         {
                                             Linknew.map((link) => (
                                                 <li key={link.name} >
-                                                    {/* <div className='bg-orange-700 w-[140px] h-11 text-center rounded-lg pt-1  '> */}
+                                         
                                                     <a href={link.link} className='font-serif text-black hover:text-orange-800 duration-500 mr-8 '>{link.name}</a>
-                                                    {/* </div> */}
+                                                
                                                 </li>
                                             ))
                                         }
@@ -208,14 +209,14 @@ const PublicNavbar = () => {
                     </div>
 
                     <Disclosure.Panel className="md:hidden">
-                        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col ">
+                        {/* <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col ">
                             <ul className={`w-full  md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-700 md:z-auto z-[-1] left-0 md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'top:8rem ' : 'top-[-490px]'}`}>
                                 {
                                     Links.map((link) => (
                                         <li key={link.name}>
-                                            {/* <div className='bg-white w-[90px] h-10 text-center rounded-lg pt-1  '> */}
+                                        
                                             <a href={link.link} className='font-serif text-black hover:text-orange-700 duration-500 mr-8 '>{link.name}</a>
-                                            {/* </div> */}
+                                       
                                         </li>
                                     ))
                                 }
@@ -225,16 +226,17 @@ const PublicNavbar = () => {
                                 {
                                     Linknew.map((link) => (
                                         <li key={link.name} >
-                                            {/* <div className='bg-white w-[90px] h-10 text-center rounded-lg pt-1  '> */}
+                                 
                                             <a href={link.link} className='font-serif text-black hover:text-orange-800 duration-500 mr-8 '>{link.name}</a>
-                                            {/* </div> */}
+                                        
                                         </li>
                                     ))
                                 }
 
                             </ul>
                             
-                        </div>
+                        </div> */}
+                        <Sidebar/>
                     </Disclosure.Panel>
                 </>
             )}
