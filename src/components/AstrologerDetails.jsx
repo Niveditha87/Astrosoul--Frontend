f0bbb804a9132e18ff4ab469a575a7233966c9b7
Aswathy N <aswathia.achu@gmail.com>
2022-11-29 11:09:23 +0530

    Astrologer details page

--- src/components/AstrologerDetails.jsx
+++ src/components/AstrologerDetails.jsx
@@ -126,9 +126,10 @@ function AstrologerDetails() {
                             </div>
                             <div className='border rounded-lg mt-3'>
                                 <div className='flex flex-col'>
-                                    <div className='  p-3 flex flex-row text-gray-500'>
+                                    <div className='  p-3 flex flex-row text-gray-500 '>
                                         <h1 className='text-gray-700'>Shweta Patel</h1> ,
                                         <p className='ml-3 text-gray-500'>20 April 2022</p>
+                                        <label className="bg-orange-800 ml-20 border rounded-md w-12 flex justify-center text-white"> <svg aria-hidden="true" className="w-5 h-5 text-yellow-400 mt-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>5</label>
                                     </div>
                                     <div className='border-b-2 text-gray-500 pl-5 pr-5 pb-5'>
                                         <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
@@ -139,6 +140,8 @@ function AstrologerDetails() {
                                     <div className='  p-3 flex flex-row text-gray-500'>
                                         <h1 className='text-gray-700'>user 1</h1> ,
                                         <p className='ml-3 text-gray-500'>20 April 2022</p>
+                                        <label className="bg-orange-800 ml-20 border rounded-md w-12 flex justify-center text-white"> <svg aria-hidden="true" className="w-5 h-5 text-yellow-400 mt-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>5</label>
+
                                     </div>
                                     <div className='border-b-2 text-gray-500 pl-5 pr-5 pb-5'>
                                         <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
@@ -149,6 +152,8 @@ function AstrologerDetails() {
                                     <div className='  p-3 flex flex-row text-gray-500'>
                                         <h1 className='text-gray-700'>user 2</h1> ,
                                         <p className='ml-3 text-gray-500'>20 April 2022</p>
+                                        <label className="bg-orange-800 ml-20 border rounded-md w-12 flex justify-center text-white"> <svg aria-hidden="true" className="w-5 h-5 text-yellow-400 mt-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>5</label>
+
                                     </div>
                                     <div className='border-b-2 text-gray-500 pl-5 pr-5 pb-5'>
                                         <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
@@ -159,6 +164,8 @@ function AstrologerDetails() {
                                     <div className='  p-3 flex flex-row text-gray-500'>
                                         <h1 className='text-gray-700'>user 3</h1> ,
                                         <p className='ml-3 text-gray-500'>20 April 2022</p>
+                                        <label className="bg-orange-800 ml-20 border rounded-md w-12 flex justify-center text-white"> <svg aria-hidden="true" className="w-5 h-5 text-yellow-400 mt-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>5</label>
+
                                     </div>
                                     <div className='border-b-2 text-gray-500 pl-5 pr-5 pb-5'>
                                         <p>survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing</p>
