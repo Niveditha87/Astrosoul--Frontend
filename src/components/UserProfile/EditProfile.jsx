4f4c07e6df962e83555dcb8af68eaed6a3ac2382
Aswathy N <aswathia.achu@gmail.com>
2022-12-06 13:07:47 +0530

    editProfile

--- src/components/UserProfile/EditProfile.jsx
+++ src/components/UserProfile/EditProfile.jsx
@@ -7,10 +7,10 @@ function EditProfile({ visible, onclose }) {
     if (!visible) return null;
     return (
         <>
-            <div className='fixed top-0 inset-0 bg-black bg-opacity-30 backdrop-blur-md flex justify-center items-center p-4'>
-                <div className='block rounded-lg bg-white md:w-[700px] mt-60 md:mt-0  '>
-                    <div className='bg-orange-500 p-2 flex justify-evenly items-center'>
-                        <div className='flex mx-auto text-xl'> <h1>Edit Your Profile</h1></div>
+            <div className='fixed top-0 inset-0  bg-opacity-30 backdrop-blur-md flex justify-center items-center p-4 '>
+                <div className='block rounded-lg bg-white md:w-[700px]    md:mt-20 h-[1000px] md:h-[500px]  '>
+                    <div className='bg-orange-500 p-2 flex justify-evenly items-center mt-52 md:mt-0'>
+                        <div className='flex mx-auto text-xl '> <h1>Edit Your Profile</h1></div>
                         <div className='flex '>  <button onClick={onclose} className=" text-xl"><AiOutlineClose /></button></div>
                       
                     </div>
@@ -113,10 +113,16 @@ function EditProfile({ visible, onclose }) {
                                         className="block w-[300px] p-2 text-indigo-700 bg-white  focus:border-indigo-900 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-20"
                                 ></input>
                             </div>
+                            </div>
+                            
                         </div>
+                        <div className='flex justify-center mb-10 bg-orange-600 w-[300px] h-[50px] mx-auto rounded-lg '>
+                            <button>Update Profile</button>
                         </div>
                     </div>
+             
                 </div>
+               
             </div>
 
         </>
